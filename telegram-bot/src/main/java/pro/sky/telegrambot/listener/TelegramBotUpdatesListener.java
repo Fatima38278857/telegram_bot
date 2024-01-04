941f5d87930f7e8513f6630a8accbbe2a05b55af
Kommit <aksaghovafatima@gmail.com>
2024-01-04 16:15:49 +0300

    telegram_bot

--- telegram-bot/src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,34 +3,72 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.model.NotificationTask;
+import pro.sky.telegrambot.repository.NotificationTaskRepository;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final TelegramBot telegramBot;
 
-    @Autowired
-    private TelegramBot telegramBot;
+    private final NotificationTaskRepository notificationTaskRepository;
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot, NotificationTaskRepository notificationTaskRepository) {
+        this.telegramBot = telegramBot;
+        this.notificationTaskRepository = notificationTaskRepository;
+    }
 
     @PostConstruct
     public void init() {
         telegramBot.setUpdatesListener(this);
     }
 
-    @Override
+    @Override  //
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            Long chatId = update.message().chat().id();
+            if (update.message().text().equals("/start")) {
+                SendMessage message = new SendMessage(chatId, "02.01.2024 20:00 Сделать домашнюю работу");
+                SendResponse response = telegramBot.execute(message);
+            }
+            Pattern pattern = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W+]+)");
+            Matcher matcher = pattern.matcher(update.message().text());
+            String date = null;
+            String item = null;
+
+            if (matcher.matches()) {
+                date = matcher.group(1);
+                item = matcher.group(3);
+                logger.info("Date: {}, item: {}", date, item);
+            }
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+            if (date != null) {
+                LocalDateTime dateTime = LocalDateTime.parse(date, formatter);
+                notificationTaskRepository.save(new NotificationTask(chatId, item, dateTime));
+                SendMessage message = new SendMessage(chatId, "Событие сохранено!");
+                telegramBot.execute(message);
+            }
         });
+
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+
+
     }
 
 }
---
+++ telegram-bot/src/main/resources/liquibase/scripts/indices.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+
+-- changeset fatima:1
+CREATE TABLE notification_task
+(
+id  bigint PRIMARY KEY,
+chat_id bigint,
+content_notification varchar,
+date_time_notification timestamp
+);
